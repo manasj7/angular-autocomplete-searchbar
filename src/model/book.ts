2e734a8ac1f760c81e17b3e1eae8e1443e42f0df
Manas <manas.jadye@gmail.com>
2021-05-20 16:50:01 +1200

    created the model class

---
+++ src/model/book.ts
@@ -0,0 +1,11 @@
+export class Book {
+    volumeInfo: [{
+      id: number;
+      title: string;
+      subtitle: string;
+      author: string;
+      description: string;
+      }];
+      constructor() {}
+    
+    }
